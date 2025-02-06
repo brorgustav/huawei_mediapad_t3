a60c6387f59b743d759222a617ab24f15093bc81
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2025-02-06 19:21:31 +0100

    Update huawei_bootloader_unlocker.c
    reboot after 4 tries

--- huawei_bootloader_unlocker.c
+++ huawei_bootloader_unlocker.c
@@ -1,42 +1,54 @@
 /*
     Bruteforce Bootloader Unlocker for HUAWEI Related smartphones
     This is intended to be the fastest ever created bootloader unlocker
+    It will reboot after 4 tries because of the code limit
 */
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#define LEN 37
+#define LEN 64
+#define ATTEMPTS_BEFORE_REBOOT 4
 
 unsigned long long base_start = 1000000000000000;
 
-void resumer(){
-    printf("\n\nLast used code was: %lld", base_start);
-	FILE * fp = fopen("lastcode","w");
-	fprintf(fp,"%llu",base_start);
-	fclose(fp);
+void resumer() {
+    printf("\n\nLast used code was: %llu", base_start);
+    FILE *fp = fopen("lastcode", "w");
+    if (fp) {
+        fprintf(fp, "%llu", base_start);
+        fclose(fp);
+    }
     exit(1);
 }
 
-int main( int argc, char **argv) {
-	if ( argc > 1 ) {
-	        char *base = argv[1];
-	        base_start = atoll( base );
-	} else {
-		FILE * fp;
-		if(fp= fopen("lastcode","w")) {
-		fscanf(fp,"%llu",&base_start);
-		fclose(fp);
-		}
-	}
-    signal(SIGINT, resumer);
-	signal(SIGTERM, resumer);
-    char fou[LEN] = "fastboot oem unlock ";
-    char TOTAL[LEN];
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        char *base = argv[1];
+        base_start = atoll(base);
+    } else {
+        FILE *fp = fopen("lastcode", "r");
+        if (fp) {
+            fscanf(fp, "%llu", &base_start);
+            fclose(fp);
+        }
+    }
 
-    while (sprintf( TOTAL, "%s%llu", fou, base_start++) && system( TOTAL ));
+    signal(SIGINT, resumer);
+    signal(SIGTERM, resumer);
+
+    char fou[] = "fastboot oem unlock ";
+    char TOTAL[LEN];
+    int attempts = 0;
+
+    while (snprintf(TOTAL, LEN, "%s%llu", fou, base_start++) < LEN && system(TOTAL)) {
+        attempts++;
+        if (attempts >= ATTEMPTS_BEFORE_REBOOT) {
+            system("fastboot reboot bootloader");
+            attempts = 0;
+        }
+    }
 
     printf("Your unlock code is: %llu", base_start);
-
-    return 1;
+    return 0;
 }
